09b8fbf3030570aad29cb33ca0b7dfcb37f97b6d
chethanakanchi9-cell <chethanakanchi9@gmail.com>
2026-09-08 10:28:04 +0530

    Add files via upload

---
+++ pythonn.ipynb.py
@@ -0,0 +1,64 @@
+
+#logical operator
+age =25
+citizen = True
+
+print(age>=18 and citizen == True)
+age = 16
+citizen = True
+
+print(age>=18 and citizen == True)
+
+has_card = False
+has_cash = True
+
+print(has_card or has_cash)
+
+is_logged_in = True
+
+print(not is_logged_in)
+
+#atm eligibility checker
+balance = 10000
+withdraw = 5000
+
+print(withdraw > 0 and withdraw <= balance)
+
+#student scholarship eligibility checker
+marks = float(input("Enter  marks: "))
+attendance = float(input("Enter attendance : "))
+
+eligible = marks >= 85 and attendance >= 75
+print("scholarship eligibility: ", eligible)
+
+#identity operators
+a = None
+
+print(a is None)
+print(a is not None)
+
+#bitwise operators
+a = 5
+b = 3
+
+print(a & b)  
+print(a | b)
+print(a ^ b)
+
+#electric city bill calculator
+units = int(input("Enter electricity units : "))
+
+rate = 6
+
+bill = units * rate
+
+print("Electricity Bill: ", bill)
+
+#travel expense calculator
+travel = float(input("Travel expense: "))
+food = float(input("Food expense: "))
+hotel = float(input("Hotel expense: "))
+
+total = travel + food + hotel
+
+print("Total Expense:", total)
